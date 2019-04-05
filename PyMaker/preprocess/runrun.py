f522994ce2985afd93c08164ad88ed4f6c9a8b62
Ubuntu <ubuntu@ip-172-26-15-70.ap-northeast-2.compute.internal>
2019-04-05 12:48:43 +0000

    little change run script

--- PyMaker/preprocess/runrun.py
+++ PyMaker/preprocess/runrun.py
@@ -10,4 +10,7 @@ import time
 #    time.sleep(10)
 
 # Code for Ojbect2 within 5000 Question URLs.
-cso.Run2(1, 100, False)
+#cso.Run2(1, 100, False)
+for i in range(2, 20):
+    cso.Run2(100 * i + 1, 100 * i + 100, False)
+    time.sleep(10)
