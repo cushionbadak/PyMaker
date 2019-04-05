649d3fe72312ecd8379919ae355887cfa4a94049
Ubuntu <ubuntu@ip-172-26-15-70.ap-northeast-2.compute.internal>
2019-04-05 12:44:22 +0000

    run Object2 for 1-100 and 101-200. It works well

---
+++ PyMaker/preprocess/runrun_test.py
@@ -0,0 +1,13 @@
+import crawlSO as cso
+import time
+
+## Code for Object1
+#for i in range(39, 226):
+#    cso.Run1(100 * i + 1, 100 * i + 100, False)
+#    ff = open("Done" + str(100 * i + 1) + '_' + str(100 * i + 100) + '.txt', 'w')
+#    ff.write("Done.")
+#    ff.close()
+#    time.sleep(10)
+
+# Code for Ojbect2 within 5000 Question URLs.
+cso.Run2(101, 200, False)
