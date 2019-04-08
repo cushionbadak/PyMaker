d87ec408efd04d52e181c041a9e882cff6e6e65b
kmg0512 <mg_kwon@naver.com>
2019-04-08 10:13:01 +0900

    get python docs html

--- PyMaker/preprocess/crawlDoc.py
+++ PyMaker/preprocess/crawlDoc.py
@@ -1,4 +1,21 @@
 import requests
+from bs4 import BeautifulSoup as bs
 
 response = requests.get("https://docs.python.org/3/contents.html")
-print(response)
+html = response.content
+contents = bs(html, "html.parser")
+
+links = []
+
+for refer in contents.find_all('a', {"class" : "reference internal"}):
+    href = refer["href"]
+    if ('#' in href) == False and links.count(href) == 0:
+        links.append(href)
+
+        response = requests.get("https://docs.python.org/3/" + href)
+        html = response.content
+        doc = bs(html, "html.parser")
+        
+        print(doc.title)
+
+        
