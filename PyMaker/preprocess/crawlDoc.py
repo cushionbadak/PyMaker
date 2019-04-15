aa847ff6fdfc1b462ad9faf7d6a82e02013015a1
kmg0512 <mg_kwon@naver.com>
2019-04-15 15:36:04 +0900

    Comments

--- PyMaker/preprocess/crawlDoc.py
+++ PyMaker/preprocess/crawlDoc.py
@@ -6,21 +6,24 @@ response = requests.get("https://docs.python.org/3/contents.html") # Python Comp
 html = response.content
 contents = bs(html, "html.parser")
 
-links = {}
-backlinks = {}
+links = {}      # Doc -> Number
+backlinks = {}  # Number -> Doc
 cnt = 0
 
+# Link to Page
 for refer in contents.find_all('a', {"class" : "reference internal"}):
     href = refer["href"]
+    # Assume Library & Reference have contents
     if (("library" in href) == True or ("reference" in href) == True) and ('#' in href) == False and links.get(href) == None:
         links[href] = cnt
         backlinks[cnt] = href
         cnt += 1
 
-        response = requests.get("https://docs.python.org/3/" + href)
+        response = requests.get("https://docs.python.org/3/" + href) # Contents Page
         html = response.content
         doc = bs(html, "html.parser")
         
+        # Link to content title
         for header in doc.find_all('a', {"class" : "headerlink"}):
             link = header["href"]
             if ('#' in link) == True and links.get(href + link) == None:
