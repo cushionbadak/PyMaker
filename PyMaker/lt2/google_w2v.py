9ec9ed4b0a0f5dd2f8981cc00103abd0daf19257
Jisuk Byun <jsbzwork@gmail.com>
2019-05-21 20:14:31 +0900

    Update google_w2v.py

--- PyMaker/lt2/google_w2v.py
+++ PyMaker/lt2/google_w2v.py
@@ -6,19 +6,20 @@ import numpy as np
 print('google_w2v.py: import google w2v model...')
 print('google_w2v.py: it will take about 2 minutes and over 2GB of memory needed to load.')
 model = gensim.models.KeyedVectors.load_word2vec_format(
-    'PyMaker/datas/GoogleNews/GoogleNews-vectors-negative300.bin', binary=True)
+    '../datas/GoogleNews/GoogleNews-vectors-negative300.bin', binary=True)
 print('google_w2v.py: finish importing google w2v model.')
+
+modellength = len(model['car'])
 
 def unigram_sentence_wordvecsum(ws):
     # ws : string list. this word list should be longer than 0.
     if len(ws) < 1:
         return None
 
-    for w in ws:
-        # make initv zero value.
-        if w in model:
-            initv = np.copy(model[w])
-            initv -= initv
+    # make initv zero value.
+    initv = np.zeros(modellength)
+    
+    print(len(model['car']))
     for w in ws:
         if w in model:
             initv += model[w]
